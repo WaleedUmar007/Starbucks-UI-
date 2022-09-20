ed15344ba7890c6d36d504430c1dcb9260392c13
Waleed Umar <waleedumar85@gmail.com>
2022-09-20 16:14:08 +0500

    added test to check responsiveness on different screen sizes

--- cypress/e2e/basicTest.cy.js
+++ cypress/e2e/basicTest.cy.js
@@ -1,4 +1,4 @@
-
+const dimension = require ("./dimension") 
 
 describe('Basic testing to see if the link is working', () => {
   it('passes', () => {
@@ -17,4 +17,24 @@ describe ('Loops through links and see if they are broken',()=>{
       })
     })
   })
+})
+
+//code to check the responsiveness of the website
+
+
+
+beforeEach(()=>{
+  cy.on('uncaught:exception',(err,runnable)=>{
+    return false
+  })
+})
+
+describe("Check the responsiveness of the website on various screensize",()=>{
+  
+    it('Scrolling',()=>{
+      cy.viewport('iphone-6+')
+      cy.visit('http://127.0.0.1:5500/index.html')
+      cy.scrollTo("bottom",{duration:30000})
+      cy.scrollTo("top",{duration:30000})
+    })
 })
---
+++ cypress/e2e/dimension.js
@@ -0,0 +1,43 @@
+module.export={
+    iphoneX:{
+        viewportWidth:375,
+        viewportHeight:812
+    },
+
+    s10:{
+        viewportWidth:360,
+        viewportHeight:760
+    },
+
+    twentyfourInch:{
+        viewportWidth:1920,
+        viewportHeight:1200
+    },
+
+    twentythreeInch:{
+        viewportWidth:1920,
+        viewportHeight:1080
+    },
+
+    twentytwoInch:{
+        viewportWidth:1680,
+        viewportHeight:1050
+    },
+
+    twentyInch:{
+        viewportWidth:1600,
+        viewportHeight:900
+    },
+
+    nineteenInch:{
+        viewportWidth:1440,
+        viewportHeight:900
+    },
+
+    fifteenInch:{
+        viewportWidth:1366,
+        viewportHeight:768
+    },
+
+
+}
